0a199aabe81843fb5f277aa6b7aca39a67aca063
Daniel Lopes <daniel.dxdb@gmail.com>
2024-03-01 16:49:16 -0300

    Creating model

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,12 +1,15 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
 
 @RestController
+@RequestMapping("customer")
 class CustomerController {
     @GetMapping
-    fun helloWorld(): String {
-        return "Hello, World"
+    fun getCostumer(): CustomerModel {
+        return CustomerModel("1", "Daniel", "daniel.dxdb@gmail.com")
     }
 }
